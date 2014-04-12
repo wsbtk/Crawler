38fd202bdedb3029132fb12c032fd3dce889f3a0
James Taylor II <jetaylorii@gmail.com>
2014-04-12 11:29:57 -0400

    Removed Database Region from Spider

--- Crawler/Spider.cs
+++ Crawler/Spider.cs
@@ -22,67 +22,6 @@ namespace Crawler
         {
             flag = go;
         }
-        #region Set Scan Information in Database
-        private string GetMd5Hash(DateTime hashTime)
-        {
-            var hash = MD5.Create();
-            var data = hash.ComputeHash(Encoding.UTF8.GetBytes(hashTime.ToString()));
-            var sBuilder = new StringBuilder();
-            for (var i = 0; i < data.Length; i++)
-                sBuilder.Append(data[i].ToString("x2"));
-            return sBuilder.ToString();
-        }
-        public void Create()
-        {
-            var conn = new MySqlConnection();
-            var cmd = new MySqlCommand();
-            conn.ConnectionString = _connString.ToString();
-            try
-            {
-                _startTime = DateTime.Now;
-                _startTimeGuid = GetMd5Hash(_startTime);
-                conn.Open();
-                cmd.Connection = conn;
-
-                cmd.CommandText = "INSERT INTO `ForagerAdmin`.`Scans` " +
-                                  "VALUES(NULL, @UserID, @site, @start_time, @end_time, @total_pages, @running, @scan_guid)";
-                cmd.Prepare();
-                cmd.Parameters.AddWithValue("@UserID", "ForagerAdmin");
-                cmd.Parameters.AddWithValue("@site", "http://www.spsu.edu/");
-                cmd.Parameters.AddWithValue("@start_time", _startTime);
-                cmd.Parameters.AddWithValue("@end_time", null);
-                cmd.Parameters.AddWithValue("@total_pages", 0);
-                cmd.Parameters.AddWithValue("@running", 1);
-                cmd.Parameters.AddWithValue("@scan_guid", _startTimeGuid);
-                cmd.ExecuteNonQuery();
-                conn.Close();
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine("Error " + ex.Number + " has occurred: " + ex.Message, "Error");
-            }
-        }
-        public int GetId()
-        {
-            var conn = new MySqlConnection();
-            var cmd = new MySqlCommand();
-            conn.ConnectionString = _connString.ToString();
-            try
-            {
-                conn.Open();
-                cmd.Connection = conn;
-                cmd.CommandText = "SELECT scan_id, start_time FROM `ForagerAdmin`.`Scans` " +
-                                  "WHERE scan_guid = '" + _startTimeGuid + "'";
-                var reader = cmd.ExecuteReader();
-                return reader.Read() ? (int)reader[0] : -1;
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine("Error " + ex.Number + " has occurred: " + ex.Message, "Error");
-                return -1;
-            }
-        }
-        #endregion
 
         public void Crawl(Uri thisUrl)
         {
